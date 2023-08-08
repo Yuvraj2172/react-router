e13c981c1a2e3b2234ae7f4f69af1f4d4be5052f
Yuvraj Soni <demonsroar2172@gmail.com>
2023-08-08 12:37:28 +0530

    first pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <h2>React Router 6 Tutorial</h2>;
-}
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>home page</div>} />
+        <Route path="about" element={<div>about page</div>} />
+        <Route path="testing" element={<div><h2>
+          testing</h2></div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
