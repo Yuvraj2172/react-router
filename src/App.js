f7b15c57360ff2646ad03393c4a659964fe070d1
Yuvraj Soni <demonsroar2172@gmail.com>
2023-08-08 14:52:15 +0530

    link pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import Home from './pages/Home';
-import About from './pages/About';
-import Products from './pages/Products';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="about" element={<About/>} />
-        <Route
-          path="products"
-          element={<Products />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
   );
 };
 
